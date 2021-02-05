a413fcbfb66368af6ce6938a70c174cd0a7ca495
remixxx31 <remiperray@gmail.com>
2021-02-05 21:11:15 +0100

    Mise en place Carroussel

--- Pages/commons/menu.php
+++ Pages/commons/menu.php
@@ -1,30 +1,30 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
+<nav class="navbar navbar-expand-lg navbar-dark perso_bgBlue">
     <a class="navbar-brand" href="#">Rémi PERRAY</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item mx-2">
+          <a class="nav-link text-white" href="#">Accueil</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Mon CV</a>
+          <a class="nav-link" href="#">Présentation</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Compétences</a>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Mon CV</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Portfolio</a>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Compétences</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Témoignage</a>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Portfolio</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Contact</a>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Témoignage</a>
+        </li>
+        <li class="nav-item mx-2">
+          <a class="nav-link" href="#">Contact</a>
         </li>
       </ul>
     </div>
-  </div>
 </nav>
--- Pages/index.php
+++ Pages/index.php
@@ -1,6 +1,31 @@
 <?php ob_start();?>
-  
-<h1>Page d'accueil</h1>
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"></li>
+    <li data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="../sources/images/cours1.png" class="d-block w-75 h-75 mx-auto" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../sources/images/cours2.png" class="d-block w-75 h-75 mx-auto" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="../sources/images/cours3.png" class="d-block w-75 h-75 mx-auto" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </a>
+</div>
+
  <button class="btn btn-success">Valider</button>
   <?php
     $content = ob_get_clean();
